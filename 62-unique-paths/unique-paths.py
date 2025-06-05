95939bad1b9eba63adedfcb3ad1001f2920aa9da
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-06-05 09:19:18 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (44.95%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,19 +1,7 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        directions=[[0,1],[1,0]]
-        memo={}
-        def dfs(r,c):
-            if r==m-1 and c==n-1:
-                return 1
-            if (r,c) in memo:
-                return memo[(r,c)]
-            count=0
-            for dr,dc in directions:
-                if (r+dr in range(m) and
-                    c+dc in range(n)):
-                    count+=dfs(r+dr,c+dc)
-            memo[(r,c)]=count
-            return count
-        return dfs(0,0)
-
-
+        dp=[1]*n
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[j]+=dp[j-1]
+        return dp[-1]
