fe9047a240adb73aff2f31c9542f6ffd90676b90
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-06-14 17:35:34 -0700

    Time: 59 ms (16.56%) | Memory: 19.7 MB (18.39%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -2,23 +2,24 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         rows,cols=len(heights),len(heights[0])
         p,a=set(),set()
-        directions=[(1,0),(0,1),(-1,0),(0,-1)]
-        def dfs(r,c,visited,preH):
-            if((r,c) in visited or
-                r not in range(rows) or 
-                c not in range(cols) or
-                heights[r][c]<preH):
+        directions=[[0,1],[0,-1],[-1,0],[1,0]]
+        def dfs(r,c,preH,visited):
+            if heights[r][c]<preH:
                 return
             visited.add((r,c))
-            
             for dr,dc in directions:
-                dfs(r+dr,c+dc,visited,heights[r][c])
+                nr,nc=r+dr,c+dc
+                if (nr in range(rows)and
+                    nc in range(cols)and
+                    (nr,nc) not in visited):
+                    dfs(r+dr,c+dc,heights[r][c],visited)
+        
+        for r in range(rows):
+            dfs(r,0,heights[r][0],p)
+            dfs(r,cols-1,heights[r][cols-1],a)
 
         for c in range(cols):
-            dfs(0,c,p,heights[0][c])
-            dfs(rows-1,c,a,heights[rows-1][c])
-        for r in range(rows):
-            dfs(r,0,p,heights[r][0])
-            dfs(r,cols-1,a,heights[r][cols-1])
-        
+            dfs(0,c,heights[0][c],p)
+            dfs(rows-1,c,heights[rows-1][c],a)
+
         return list(p&a)
