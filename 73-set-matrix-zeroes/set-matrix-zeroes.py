79266686f4f8e60075c82786adb2deec6b558746
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-06-18 22:18:55 -0700

    Time: 3 ms (69.60%) | Memory: 18.5 MB (52.82%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,22 +4,24 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         rows,cols=len(matrix),len(matrix[0])
-        c0=any(matrix[r][0]==0 for r in range(rows))
-        r0=any(matrix[0][c]==0 for c in range(cols))
+        r0=any(matrix[r][0]==0 for r in range(rows))
+        c0=any(matrix[0][c]==0 for c in range(cols))
+            
         for r in range(1,rows):
             for c in range(1,cols):
                 if matrix[r][c]==0:
-                    matrix[r][0]=0
-                    matrix[0][c]=0
-
+                    matrix[0][c]=matrix[r][0]=0
+        
         for r in range(1,rows):
             for c in range(1,cols):
                 if matrix[r][0]==0 or matrix[0][c]==0:
                     matrix[r][c]=0
         
         if r0:
-            for c in range(cols):
-                matrix[0][c]=0
-        if c0:
             for r in range(rows):
                 matrix[r][0]=0
+        if c0:
+            for c in range(cols):
+                matrix[0][c]=0
+
+                    
