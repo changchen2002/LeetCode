efe9ba7a7e7b673627ca8a1a8b33f9aeec1ccc57
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-06-23 21:05:38 -0700

    Time: 10 ms (62.33%) | Memory: 17.8 MB (61.45%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,16 +1,18 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res=[]
-        def dfs(path,start,total):
-            if total==target:
-                res.append(path[:])
-            if total>target:
+        def dfs(i,total,curArray):
+            if i>=len(candidates) or total>target:
                 return
+            if total==target:
+                res.append(curArray[:])
+                return 
 
-            for i in range(start,len(candidates)):
-                path.append(candidates[i])
-                dfs(path,i,total+candidates[i])
-                path.pop()
-        dfs([],0,0)
+            curArray.append(candidates[i])
+            dfs(i,total+candidates[i],curArray)
+            curArray.pop()
+
+            dfs(i+1,total,curArray)
+        dfs(0,0,[])
         return res
-
+            
