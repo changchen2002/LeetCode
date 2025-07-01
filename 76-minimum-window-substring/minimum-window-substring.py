9cd7c074b9dfa4812466fc47c255bd7459eb54c4
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-07-01 12:06:59 -0700

    Time: 135 ms (30.90%) | Memory: 18.3 MB (34.53%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,30 +1,26 @@
-from collections import Counter
-
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s)<len(t):
-            return ""
         need=Counter(t)
-        window={}
-        have,needCount=0,len(need)
+        have=l=0
         res=""
-        l=0
-
+        window=defaultdict(int)
         for r in range(len(s)):
             c=s[r]
-            window[c]=window.get(c,0)+1
-
-            if c in need and window[c]==need[c]:
-                have+=1
-            
-            while have==needCount:
-                if not res or (r-l+1)<len(res):
-                    res=s[l:r+1]
+            if c in need:
+                window[c]+=1
+                if window[c]==need[c]:
+                    have+=1
                 
-                window[s[l]]-=1
-                if s[l] in need and window[s[l]]<need[s[l]]:
-                    have-=1
+            while have==len(need):
+                if not res or r-l+1<len(res):
+                    res=s[l:r+1]
+                a=s[l]
+                if a in need:
+                    if window[a]==need[a]:
+                        have-=1
+                    window[a]-=1
                 l+=1
-        
+                
         return res
-        
+            
+            
