0a4895d8ea98224996c3b6858684d449ee1f6927
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-08-01 18:05:39 -0700

    Time: 3 ms (70.02%) | Memory: 17.6 MB (97.71%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,14 +1,9 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-    # DP: use Top-Down (recursion + memoization) unless: 	
-	# The problem is clearly iterative in nature (e.g. coin change, knapsack)
-    # this is like coin change, so we use bottom up
-
-        dp=[True]+ [False]*len(s) #set a beginning and set it true
-        for i in range(1,len(s)+1):
+        dp=[False]*(len(s)+1)
+        dp[0]=True
+        for i in range(len(s)+1):
             for w in wordDict:
-                start=i-len(w)
-                if start>=0 and dp[start] and s[start:i]==w:
-                    dp[i]=True
-                    break
-        return dp[-1] #dont mind about the index
+                if i>=len(w) and s[i-len(w):i]==w and dp[i-len(w)]:
+                    dp[i]=dp[i-len(w)]
+        return dp[-1]
