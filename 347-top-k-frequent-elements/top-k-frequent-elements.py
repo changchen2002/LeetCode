9ac4063e896e2e20209fbe38f97d64154acb9618
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-08-10 20:07:21 -0700

    Time: 7 ms (50.62%) | Memory: 24.3 MB (7.55%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,19 +1,12 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        #堆查找时间O(n)
-        hashmap={}
-        for num in nums:
-            if num in hashmap:
-                hashmap[num]+=1
-            else:
-                hashmap[num]=1
-        bucket=[[]for i in range(len(nums)+1)] #index+1
-        for num,freq in hashmap.items():
-            bucket[freq].append(num) #maxFreqIndex=len(nums)
-
+        bucket=[[] for i in range(len(nums)+1)]
+        count=Counter(nums)
+        for num,freq in count.items():
+            bucket[freq].append(num)
         res=[]
-        for freq in range(len(bucket)-1,0,-1): #遍历到0因为bucket里频率最小为0
-            for num in bucket[freq]:
-                res.append(num)
+        for i in range(len(bucket)-1,-1,-1):
+            for n in bucket[i]:
+                res.append(n)
                 if len(res)==k:
                     return res
