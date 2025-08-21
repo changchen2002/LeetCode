92fa44533b84366f1b5e7e742559acf115daa16a
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-08-21 16:20:21 -0700

    Time: 201 ms (56.69%) | Memory: 26.3 MB (60.62%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,22 +1,21 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
         l=res=0
-        maxDeque=deque()
-        minDeque=deque()
+        incq=deque()
+        decq=deque()
         for r in range(len(nums)):
-            while maxDeque and nums[r]>maxDeque[-1]:
-                maxDeque.pop()
-            maxDeque.append(nums[r])
-            while minDeque and nums[r]<minDeque[-1]:
-                minDeque.pop()
-            minDeque.append(nums[r])
-                
-            while maxDeque[0]-minDeque[0]>limit:
-                if nums[l]==maxDeque[0]:
-                    maxDeque.popleft()
-                if nums[l]==minDeque[0]:
-                    minDeque.popleft()
+            while incq and incq[-1]>nums[r]:
+                incq.pop()
+            incq.append(nums[r])
+            while decq and decq[-1]<nums[r]:
+                decq.pop()
+            decq.append(nums[r])
+
+            while decq[0]-incq[0]>limit:
+                if nums[l]==decq[0]:
+                    decq.popleft()
+                if nums[l]==incq[0]:
+                    incq.popleft()
                 l+=1
-            
             res=max(res,r-l+1)
         return res
