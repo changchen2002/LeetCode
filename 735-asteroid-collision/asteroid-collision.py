fe5a18af6a6aa7fda287e8cadd9f2fffad646642
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-08-21 16:47:39 -0700

    Time: 7 ms (58.79%) | Memory: 19 MB (19.63%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,16 +1,14 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        stack = []
+        stack=[]
         for a in asteroids:
-            # 处理向左移动的负数
-            while stack and a < 0 < stack[-1]:
-                if stack[-1] < -a:
-                    stack.pop()  # 栈顶炸了，继续比
-                    continue
-                elif stack[-1] == -a:
-                    stack.pop()  # 同时炸
-                break  # 当前 a 被炸掉（或同归于尽），不入栈
-            else:
-                # 没有碰撞 或 a 是正数 或 栈为空
+            if stack and stack[-1]>0 and a<0:
+                while stack and stack[-1]>0 and a<0:
+                    top=stack.pop()
+                    if abs(a)<top:
+                        a=top
+                    elif abs(a)==top:
+                        a=0
+            if a!=0:
                 stack.append(a)
         return stack
