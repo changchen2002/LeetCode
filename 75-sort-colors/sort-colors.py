17dc1a02a9c348f281e7d9635292725f78ab7fdd
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-08-21 15:54:51 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (53.76%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -5,12 +5,12 @@ class Solution:
         """
         l,r,i=0,len(nums)-1,0
         while i<=r:
-            if nums[i]==2:
-                nums[i],nums[r]=nums[r],nums[i]
-                r-=1
-            elif nums[i]==0:
+            if nums[i]==0:
                 nums[i],nums[l]=nums[l],nums[i]
                 l+=1
                 i+=1
             elif nums[i]==1:
                 i+=1
+            else:
+                nums[i],nums[r]=nums[r],nums[i]
+                r-=1
