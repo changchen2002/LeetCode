34f593ead71896088fe68be5489b472fc6c0e99f
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-09-03 16:50:19 -0700

    Time: 72 ms (68.93%) | Memory: 21.5 MB (78.75%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        mod=10**9+7
+        stack=[]
+        total=0
+        for i in range(len(arr)+1):
+            while stack and (i==len(arr) or arr[stack[-1]]>=arr[i]):
+                mid=stack.pop() 
+                left=-1 if not stack else stack[-1]
+                right=i
+                count=(mid-left)*(right-mid)
+                total+=count*arr[mid]
+            stack.append(i)
+        return total%mod
