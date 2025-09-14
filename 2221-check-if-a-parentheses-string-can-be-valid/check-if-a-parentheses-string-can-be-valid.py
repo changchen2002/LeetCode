db014130d1f3225a6b8c153679a48140acedb237
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-09-13 21:23:11 -0700

    Time: 91 ms (52.84%) | Memory: 18.3 MB (72.51%) - LeetSync

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canBeValid(self, s: str, locked: str) -> bool:
+        if len(s)%2:
+            return False
+
+        l=count=0
+        for p,lock in zip(s,locked):
+            if lock=='0':
+                count+=1
+            elif p=='(':
+                l+=1
+            elif p==')':
+                if l>0:
+                    l-=1
+                elif count>0:
+                    count-=1
+                else:
+                    return False
+        
+        balance=0
+        for i in range(len(s)-1,-1,-1):
+            if locked[i]=='0':
+                balance-=1
+                count-=1
+            elif s[i]=='(':
+                balance+=1
+                l-=1
+            elif s[i]==')':
+                balance-=1
+            if balance>0:
+                return False
+        return balance<=0
