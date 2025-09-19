27c6f7568153d47aafeb13225f952cdd95f9b7a6
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-09-18 23:09:43 -0700

    Time: 69 ms (19.57%) | Memory: 93 MB (15.87%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,9 @@
+#include <unordered_set>
+using namespace std;
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st(nums.begin(),nums.end());
+        return st.size()!=nums.size();
+    }
+};
