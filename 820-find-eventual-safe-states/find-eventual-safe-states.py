18332a6907f5d759739d3a62b875aef9e779a211
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-04 13:38:17 -0700

    Time: 29 ms (80.59%) | Memory: 23.5 MB (85.23%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -1,21 +1,15 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        adj=defaultdict(list)
         n=len(graph)
-        for u in range(n):
+        color=[0]*n
+        def dfs(u):
+            if color[u]>0:
+                return color[u]==2
+            color[u]=1
             for v in graph[u]:
-                adj[v].append(u)
-        res=[i for i in range(n) if len(graph[i])==0]
-        q=deque(res) 
-        visit=set() 
-        while q:
-            des=q.popleft() 
-            visit.add(des) 
-            for src in adj[des]:
-                graph[src].remove(des)
-                if len(graph[src])==0 and src not in visit:
-                    res.append(src)
-                    q.append(src)
-        return sorted(res)
+                if color[v]==1 or not dfs(v):
+                    return False
+            color[u]=2
+            return True
         
-
+        return [i for i in range(n) if dfs(i)]
