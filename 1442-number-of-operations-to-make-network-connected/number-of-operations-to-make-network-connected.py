a690115e3d94085a1177efadbfcff3b2051344e2
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-04 18:36:50 -0700

    Time: 43 ms (53.36%) | Memory: 44.2 MB (5.59%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
@@ -2,20 +2,21 @@ class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
         if len(connections)<n-1:
             return -1
-        parent=[i for i in range(n)]
-        def find(x):
-            if parent[x]!=x:
-                parent[x]=find(parent[x])
-            return parent[x]
-        def union(x,y):
-            px,py=find(x),find(y)
-            if px!=py:
-                parent[px]=py
-                return 1
-            return 0
+        adj=defaultdict(list)
+        for u,v in connections:
+            adj[u].append(v)
+            adj[v].append(u)
+        vis=set()
+        def dfs(u):
+            vis.add(u)
+            for v in adj[u]:
+                if v not in vis:
+                    vis.add(v)
+                    dfs(v)
 
-        comp=n
-        for a,b in connections:
-            comp-=union(a,b) #减少一个连通块
+        comp=0
+        for i in range(n):
+            if i not in vis:
+                comp+=1
+                dfs(i)
         return comp-1
-            
