9b9cea117b48c448e725e7419ebd465783e55829
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-04 21:39:21 -0700

    Time: 111 ms (70.96%) | Memory: 44.4 MB (73.42%) - LeetSync

--- 1308-smallest-string-with-swaps/smallest-string-with-swaps.py
+++ 1308-smallest-string-with-swaps/smallest-string-with-swaps.py
@@ -1,28 +1,24 @@
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
-        if not pairs:
-            return s
         n=len(s)
-        adj=defaultdict(list)
-        for u,v in pairs:
-            adj[u].append(v)
-            adj[v].append(u)
-        
-        vis=set()
-        res=list(s)
+        par=[i for i in range(n)]
 
-        def dfs(u,comp):
-            vis.add(u)
-            comp.append(u)
-            for v in adj[u]:
-                if v not in vis:
-                    dfs(v,comp)
+        def find(x):
+            if par[x]!=x:
+                par[x]=find(par[x])
+            return par[x]
+        def union(x,y):
+            par[find(x)]=find(y)
+        for u,v in pairs:
+            union(u,v)
+        
+        groups=defaultdict(list)
         for i in range(n):
-            comp=[]
-            dfs(i,comp)
-            chars=sorted([res[j] for j in comp])
-            for c,idx in zip(chars,sorted(comp)):
-                res[idx]=c
-        return "".join(res)
+            groups[find(i)].append(i)
         
-        
+        res=list(s)
+        for idxs in groups.values():
+            chars=sorted([res[i] for i in idxs])
+            for i,c in zip(idxs,chars):
+                res[i]=c
+        return ''.join(res)
