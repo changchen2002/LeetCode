68962677fd56e8b6edcc3914771441c6162c1ba1
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-06 15:28:41 -0700

    Time: 19 ms (95.92%) | Memory: 24.5 MB (18.75%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.py
+++ 922-possible-bipartition/possible-bipartition.py
@@ -4,19 +4,18 @@ class Solution:
         for a,b in dislikes:
             adj[a].append(b)
             adj[b].append(a)
-        
-        color={}
+        color=[0]*(n+1)
         def dfs(node,c):
-            if node in color:
-                return color[node]==c
             color[node]=c
             for nei in adj[node]:
-                if not dfs(nei,1-c):
+                if color[nei]==c:
+                    return False
+                if color[nei]==0 and not dfs(nei,-c):
                     return False
             return True
         
-        for node in range(1,n+1):
-            if node not in color:
-                if not dfs(node,0):
-                    return False
+        for i in range(1,n+1):
+            if color[i]==0 and not dfs(i,1):
+                return False
         return True
+                
