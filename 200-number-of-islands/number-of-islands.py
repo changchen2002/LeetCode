58314a47af685b3faa25466335c44389ae97da69
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-06 14:42:21 -0700

    Time: 241 ms (67.91%) | Memory: 20 MB (95.44%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,21 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        res=0
         rows,cols=len(grid),len(grid[0])
-        directions=[[1,0],[-1,0],[0,1],[0,-1]]
-        
-        def dfs(r,c):
-            grid[r][c]='0'
-            for dr,dc in directions:
-                nr,nc=r+dr,c+dc
-                if (nr in range(rows) and 
-                    nc in range(cols) and
-                    grid[nr][nc]=='1'):
-                    dfs(nr,nc)
-
+        directions=[(0,1),(0,-1),(1,0),(-1,0)]
+        q=deque()
+        def bfs(r,c):
+            q.append((r,c))
+            while q:
+                for _ in range(len(q)):
+                    r,c=q.popleft()
+                    for dr,dc in directions:
+                        nr,nc=r+dr,c+dc
+                        if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]=="1":
+                            grid[nr][nc]="0"
+                            q.append((nr,nc))
+        res=0
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c]=='1':
-                    dfs(r,c)
+                if grid[r][c]=="1":
                     res+=1
+                    grid[r][c]="0"
+                    bfs(r,c)
         return res
