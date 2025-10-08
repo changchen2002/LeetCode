e0058dcdac3973bcb1426525844e1990e9a0de58
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-08 12:59:32 -0700

    Time: 41 ms (50.99%) | Memory: 26.7 MB (76.73%) - LeetSync

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
@@ -2,23 +2,11 @@ class Solution:
     def minNumberOperations(self, target: List[int]) -> int:
         if not target:
             return 0
-        stack=[min(target)]
-        res=min(target)
-        for n in target:
-            while stack[-1]>n:
-                top=stack.pop()
-                res+=top-max(stack[-1],n)
-            stack.append(n)
-        
-        for i in range(1,len(stack)):
-            res+=stack[i]-stack[i-1]
+        res=target[0]
+        for i in range(1,len(target)):
+            if target[i]>target[i-1]:
+                res+=target[i]-target[i-1]
         return res
 
-# stack   n    res
-# [1,1]    1     1
-# [1,1,2]   2     1
-# [1,1,2,3] 3     1
-# [1,1,2]   2     2
-# [1,1,1]  1     3
 
 
