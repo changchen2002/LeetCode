bcaea46f5ac402a0796b58bcd92f2b68bb0bb8e6
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-15 12:09:15 -0700

    Time: 3 ms (95.62%) | Memory: 18.1 MB (44.73%) - LeetSync

--- 265-paint-house-ii/paint-house-ii.py
+++ 265-paint-house-ii/paint-house-ii.py
@@ -2,12 +2,19 @@ class Solution:
     def minCostII(self, costs: List[List[int]]) -> int:
         n,k=len(costs),len(costs[0])
         for i in range(1,n):
+            min1=min2=float('inf')
+            idx1=-1
             for j in range(k):
-                mnm=min(costs[i-1][color] for color in range(k) if color!=j)
-                costs[i][j]+=mnm
+                c=costs[i-1][j]
+                if c<min1:
+                    min1,min2,idx1=c,min1,j
+                elif c<min2:
+                    min2=c
+            for j in range(k):
+                if j==idx1:
+                    costs[i][j]+=min2
+                else:
+                    costs[i][j]+=min1
                         
         return min(costs[-1])
         
-
-        #[1,5,3]
-        #[2+min(5,3),9+min(1,3),4+min(1,5)]
