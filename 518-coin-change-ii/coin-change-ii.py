ad1d56b8b7dc9ebaa30cad48945fd991a9c22c1b
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-15 19:15:25 -0700

    Time: 195 ms (69.95%) | Memory: 17.8 MB (98.48%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -2,7 +2,7 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         dp=[0]*(amount+1)
         dp[0]=1
-        for c in coins:
-            for a in range(c,amount+1):
+        for c in coins:  #这个顺序是求有多少种组合
+            for a in range(c,amount+1): #a和c顺序反过来则是求有多少种不同顺序
                 dp[a]+=dp[a-c]
         return dp[amount]
