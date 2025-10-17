6b1dbee40beeaa5a0d4cc528f5e9f3178acfb5cd
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-17 10:09:42 -0700

    Time: 3 ms (49.97%) | Memory: 19.6 MB (60.39%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,16 +1,28 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        if not intervals:
+            return [newInterval]
         res=[]
-        for i in range(len(intervals)):
-            if intervals[i][1]<newInterval[0]:
-                res.append(intervals[i])
-            elif intervals[i][0]>newInterval[1]:
+        def merge(i1,i2):
+            mnm=min(i1[0],i2[0])
+            mxm=max(i1[1],i2[1])
+            return [mnm,mxm]
+
+        for i,(s,e) in enumerate(intervals):
+            if newInterval[1]<s:
                 res.append(newInterval)
-                res.extend(intervals[i:])
+                res+=intervals[i:]
                 return res
+            elif newInterval[0]>e:
+                res.append(intervals[i])
+                if i==len(intervals)-1:
+                    res.append(newInterval)
+                    return res
+                continue
             else:
-                newInterval=[min(intervals[i][0],newInterval[0]), max(intervals[i][1],newInterval[1])]
-        res.append(newInterval)
-        return res
-                
-            
+                newInterval=merge(newInterval,intervals[i])
+                if i==len(intervals)-1:
+                    res.append(newInterval)
+                    return res
+
+#[1,2] [5,6] [3,6]
