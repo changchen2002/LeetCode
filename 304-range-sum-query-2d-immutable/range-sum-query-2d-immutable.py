961e4bf62b5718877965ba91e6f0e3c36dd8641c
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-16 18:16:43 -0700

    Time: 116 ms (53.07%) | Memory: 30.9 MB (67.31%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,28 +1,28 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.m=len(matrix)
-        self.n=len(matrix[0])
-        self.matrix=[[0]*self.n for _ in range(self.m)]
-        for r in range(self.m):
-            for c in range(self.n):
-                self.matrix[r][c]=matrix[r][c]
-
-        self.prefix=[[0]*self.n for _ in range(self.m)]
-        for r in range(self.m):
-            cur=0
-            for c in range(self.n):
-                cur+=self.matrix[r][c]
-                self.prefix[r][c]=cur
+        if not matrix:
+            return
+        m,n=len(matrix),len(matrix[0])
+        self.prefix=[[0]*(n+1) for _ in range(m+1)]
+        
+        for r in range(m):
+            for c in range(n):
+                self.prefix[r+1][c+1]=(
+                    self.prefix[r][c+1]+
+                    self.prefix[r+1][c]-
+                    self.prefix[r][c]+
+                    matrix[r][c]
+                )
         
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        res=0
-        for r in range(row1,row2+1):
-            if col1>0:
-                res+=self.prefix[r][col2]-self.prefix[r][col1-1]
-            else:
-                res+=self.prefix[r][col2]
-        return res
+        p=self.prefix        
+        return (
+            p[row2+1][col2+1]-
+            p[row1][col2+1]-
+            p[row2+1][col1]+
+            p[row1][col1]
+        )
 
 
 
