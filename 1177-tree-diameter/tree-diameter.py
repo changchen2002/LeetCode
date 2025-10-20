1e24d512429eea84d5414611546bb2ddacc2ab5f
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-20 12:47:28 -0700

    Time: 24 ms (29.37%) | Memory: 21.8 MB (86.62%) - LeetSync

--- 1177-tree-diameter/tree-diameter.py
+++ 1177-tree-diameter/tree-diameter.py
@@ -6,23 +6,24 @@ class Solution:
         for u,v in edges:
             adj[u].append(v)
             adj[v].append(u)
-        nodes=len(adj)
-        def dfs(start):
-            vis=[-1]*nodes
-            vis[start]=0
-            q=deque([start])
-            while q:
-                node=q.popleft()
-                for nei in adj[node]:
-                    if vis[nei]==-1:
-                        vis[nei]=vis[node]+1
-                        q.append(nei)
-
-            length=max(vis)
-            node=vis.index(length)
-            return length,node
-        _,a=dfs(0)
-        res,_=dfs(a)
-        return res
-
         
+        def bfs(node):
+            q=deque([node])
+            vis=set()
+            edge=-1
+            while q:
+                edge+=1
+                for _ in range(len(q)):
+                    node=q.popleft()
+                    vis.add(node)
+                    for nei in adj[node]:
+                        if nei not in vis:
+                            q.append(nei)
+            return node,edge
+        
+        n1,e1=bfs(edges[0][0])
+        n2,res=bfs(n1)
+        return res
+        
+
+
