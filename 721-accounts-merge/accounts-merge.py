c0825b15e63693af2423467a7f2f999cb1af3cb9
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-20 10:21:05 -0700

    Time: 28 ms (58.27%) | Memory: 20.6 MB (82.77%) - LeetSync

--- 721-accounts-merge/accounts-merge.py
+++ 721-accounts-merge/accounts-merge.py
@@ -1,32 +1,33 @@
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        email2acc={}
         parent={}
-        emailToName={}
-
+        for acc in accounts:
+            for email in acc[1:]:
+                parent[email]=email
+                email2acc[email]=acc[0]  
+        
         def find(x):
             if parent[x]!=x:
                 parent[x]=find(parent[x])
             return parent[x]
-
         def union(x,y):
-            parent[find(x)]=find(y)
-        
-        for a in accounts:
-            name=a[0]
-            first=a[1]
-            for email in a[1:]:
-                if email not in parent:
-                    parent[email]=email
-                union(first,email)
-                emailToName[email]=name
+            parent[find(x)]=find(y) #有问题
 
-        roots=defaultdict(list)
+        for acc in accounts:
+            first=acc[1]
+            for email in acc[2:]:  #有问题
+                union(email,first)
+
+        emails=defaultdict(list)
         for email in parent:
-            root=find(email)
-            roots[root].append(email)
+            emails[find(email)].append(email) #有问题
         
         res=[]
-        for root,emails in roots.items():
-            name=emailToName[root]
-            res.append([name]+sorted(emails))
+        for root in emails:
+            acc=email2acc[root]
+            res.append([acc]+sorted(emails[root]))  #有问题
+        
         return res
+
+
