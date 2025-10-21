a7e4030619e8f52d50a1e3da62cd0f6a48295276
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-20 17:16:20 -0700

    Time: 11 ms (97.58%) | Memory: 18.4 MB (40.55%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -3,14 +3,13 @@ class Solution:
         m,n=len(grid),len(grid[0])
         directions=[[0,1],[0,-1],[1,0],[-1,0]]
         res=0
-        vis=set()
         def dfs(r,c):
             nonlocal res
-            vis.add((r,c))
+            grid[r][c]=0
             cur=1
             for dr,dc in directions:
                 nr,nc=r+dr,c+dc
-                if 0<=nr<m and 0<=nc<n and (nr,nc) not in vis and grid[nr][nc]==1:
+                if 0<=nr<m and 0<=nc<n and grid[nr][nc]==1:
                     cur+=dfs(nr,nc)
             res=max(res,cur)
             return cur
