a4d895b885d3486df3191dd197ea1c0f1c09180e
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-21 08:42:23 -0700

    Time: 125 ms (85.85%) | Memory: 20.4 MB (57.94%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -7,22 +7,16 @@ class Solution:
         def dfs(r,c):
             if dp[r][c]:
                 return dp[r][c]
-            cur=1
+            add=0
             for dr,dc in directions:
                 nr,nc=r+dr,c+dc
                 if 0<=nr<m and 0<=nc<n and matrix[nr][nc]>matrix[r][c]:
-                    cur=max(cur,1+dfs(nr,nc))
-            dp[r][c]=cur
-            return cur
+                    add=max(add,dfs(nr,nc))
+            dp[r][c]=add+1
+            return dp[r][c]
         
         res=0
         for r in range(m):
             for c in range(n):
                 res=max(res,dfs(r,c))
         return res
-
-# r,c      cur         add         dp[r][c]   
-# 2,1       4           3          dp[2][1]=4
-# 2,0       3            2         dp[2][0]=3
-# 1,0       2           1          dp[1,0]=2     
-# 0,0       1          0           dp[0][0]=1
