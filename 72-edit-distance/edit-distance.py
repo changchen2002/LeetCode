d2a58611f38114e85b46aa8ce4a935feeb0459e2
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-20 21:13:03 -0700

    Time: 51 ms (75.21%) | Memory: 18.1 MB (93.34%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,14 +1,19 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        if len(word1)<len(word2):
+        m,n=len(word1),len(word2)
+        if m>n:
             word1,word2=word2,word1
-        prev=list(range(len(word2)+1)) 
-        for i,c1 in enumerate(word1,1):
+            m,n=n,m
+        dp=list(range(n+1))
+
+        for i in range(1,m+1):
             cur=[i]
-            for j,c2 in enumerate(word2,1):
-                if c1==c2:
-                    cur.append(prev[j-1])
+            for j in range(1,n+1):
+                if word1[i-1]==word2[j-1]:
+                    cur.append(dp[j-1])
                 else:
-                    cur.append(1+min(prev[j],cur[-1],prev[j-1]))
-            prev=cur
-        return prev[-1]
+                    cur.append(1+min(dp[j],cur[-1],dp[j-1]))
+            dp=cur
+        return dp[-1]
+
+
