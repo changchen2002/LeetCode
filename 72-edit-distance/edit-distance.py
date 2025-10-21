e5dfbe07be96f87521b411153e237c2cb2e6a0bc
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-20 21:14:29 -0700

    Time: 47 ms (78.16%) | Memory: 17.9 MB (94.89%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -4,16 +4,16 @@ class Solution:
         if m>n:
             word1,word2=word2,word1
             m,n=n,m
-        dp=list(range(n+1))
+        dp=list(range(n+1))  #上一排
 
         for i in range(1,m+1):
-            cur=[i]
+            cur=[i]   #这一排
             for j in range(1,n+1):
                 if word1[i-1]==word2[j-1]:
-                    cur.append(dp[j-1])
+                    cur.append(dp[j-1])  #上一排前一个
                 else:
-                    cur.append(1+min(dp[j],cur[-1],dp[j-1]))
-            dp=cur
+                    cur.append(1+min(dp[j],cur[-1],dp[j-1]))  #上一排这一个,这一排上一个,上一排前一个
+            dp=cur #上一排=这一排
         return dp[-1]
 
 
