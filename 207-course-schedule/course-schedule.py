69288bf23e5a74d215bbcc4bd9217dfcc91dc95c
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-20 20:22:52 -0700

    Time: 4 ms (67.59%) | Memory: 19.4 MB (40.67%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,33 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        if numCourses<2:
-            return True
         adj=defaultdict(list)
-        for u,v in prerequisites:
-            adj[u].append(v)
+        for a,b in prerequisites:
+            adj[a].append(b)
         
-        vis=set()
         def dfs(c):
             if c in vis:
                 return False
             if adj[c]==[]:
                 return True
             vis.add(c)
-            for p in adj[c]:
-                if not dfs(p):
+            for pre in adj[c]:
+                if not dfs(pre):
                     return False
             adj[c]=[]
             vis.remove(c)
             return True
-
-        for i in range(numCourses):
-            if not dfs(i):
+        
+        vis=set()
+        for c in range(numCourses):
+            if not dfs(c):
                 return False
         return True
-
-# i    c    p   return 
-# 0    0   []   True
-# 1    1    4    True   adj[1]=[]
-# 2    2    4    True   adj[2]=[]
-# 3    3    1    True   
-# 3    3    2    True
+            
