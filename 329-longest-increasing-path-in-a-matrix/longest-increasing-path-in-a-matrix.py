3b7b76e1c7ea99c9a5a23e8912a3641157057a3d
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-20 17:01:49 -0700

    Time: 128 ms (81.96%) | Memory: 20.7 MB (52.31%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -3,20 +3,26 @@ class Solution:
         m,n=len(matrix),len(matrix[0])
         dp=[[0]*n for _ in range(m)]
         directions=[[0,1],[0,-1],[-1,0],[1,0]]
-        
-        def dfs(r,c,prev):
-            if r not in range(m) or c not in range(n) or matrix[r][c]<=prev:
-                return 0
-            if dp[r][c]!=0:
-                return dp[r][c]
-            best=1
-            for dr,dc in directions:
-                best=max(best,1+dfs(r+dr,c+dc,matrix[r][c]))
-            dp[r][c]=best
-            return best
 
+        def dfs(r,c):
+            if dp[r][c]:
+                return dp[r][c]
+            cur=1
+            for dr,dc in directions:
+                nr,nc=r+dr,c+dc
+                if 0<=nr<m and 0<=nc<n and matrix[nr][nc]>matrix[r][c]:
+                    cur=max(cur,1+dfs(nr,nc))
+            dp[r][c]=cur
+            return cur
+        
         res=0
         for r in range(m):
             for c in range(n):
-                res=max(res,dfs(r,c,float(-inf)))
+                res=max(res,dfs(r,c))
         return res
+
+# r,c      cur         add         dp[r][c]   
+# 2,1       4           3          dp[2][1]=4
+# 2,0       3            2         dp[2][0]=3
+# 1,0       2           1          dp[1,0]=2     
+# 0,0       1          0           dp[0][0]=1
