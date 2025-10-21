3d36c51b1db06df91b06b7018d49abb278a258de
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-20 20:39:27 -0700

    Time: 3 ms (76.44%) | Memory: 18.9 MB (95.09%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,27 +1,20 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        #adj list(pre->course), for pre indegree==0 we take it first,
-        #mark pre as vis, for c, if c is vis, return False. else: cou.outdegree-1, if outdegree==0, take it. in the end if outdegree, return False
-        if numCourses<2:
-            return [0]
-        
         adj=defaultdict(list)
-        indeg=[0]*numCourses
-        for c,p in prerequisites:
-            adj[p].append(c)
-            indeg[c]+=1
-
-        res=[i for i in range(numCourses) if indeg[i]==0]
-        q=deque(res)
+        out=[0]*numCourses
+        for a,b in prerequisites:
+            adj[b].append(a)
+            out[a]+=1
         
+        res=[]
+        q=deque([n for n in range(numCourses) if out[n]==0])
         while q:
-            p=q.popleft()
-            for c in adj[p]:
-                indeg[c]-=1
-                if indeg[c]==0:
-                    res.append(c)
-                    q.append(c)
-        return res if len(res)==numCourses else []
+            pre=q.popleft()
+            res.append(pre)
+            for course in adj[pre]:
+                out[course]-=1
+                if out[course]==0:
+                    q.append(course)
 
-# adj[0]=[1]
-# indeg[0]=1
+        return res if len(res)==numCourses else [] 
+
