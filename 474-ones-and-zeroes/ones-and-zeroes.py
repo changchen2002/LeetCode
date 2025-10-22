b41f3940adead1243ec85b56d2c558ef10d3f400
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-22 11:49:00 -0700

    Time: 1567 ms (63.02%) | Memory: 18 MB (95.78%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.py
+++ 474-ones-and-zeroes/ones-and-zeroes.py
@@ -1,21 +1,32 @@
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        hashmap=defaultdict()
-        for s in strs:
-            zero,one=0,0
-            for c in s:
-                if c=='1':
-                    one+=1
-                else:
-                    zero+=1
-            hashmap[s]=(zero,one)
-
         dp=[[0]*(n+1) for _ in range(m+1)]
-
-        for s in strs:  #for key in hashmap 不要这样.key是什么不清晰
-            zero,one=hashmap[s]
+        
+        for s in strs:
+            zero=s.count('0')
+            one=s.count('1')
             for i in range(m,zero-1,-1):
                 for j in range(n,one-1,-1):
-                    dp[i][j]=max(dp[i][j],dp[i-zero][j-one]+1)
+                    dp[i][j]=max(dp[i][j],dp[i-zero][j-one]+1)            
+        return dp[-1][-1]
+        
+        
+        # hashmap=defaultdict()
+        # for s in strs:
+        #     zero,one=0,0
+        #     for c in s:
+        #         if c=='1':
+        #             one+=1
+        #         else:
+        #             zero+=1
+        #     hashmap[s]=(zero,one)  #相同字符串被覆盖掉了
 
-        return dp[-1][-1]   
+        # dp=[[0]*(n+1) for _ in range(m+1)]
+
+        # for key in hashmap:  #for key in hashmap 相同字符串会少一项
+        #     zero,one=hashmap[key]
+        #     for i in range(m,zero-1,-1):
+        #         for j in range(n,one-1,-1):
+        #             dp[i][j]=max(dp[i][j],dp[i-zero][j-one]+1)
+
+        # return dp[-1][-1]   
