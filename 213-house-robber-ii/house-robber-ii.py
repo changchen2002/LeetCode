9182271bc0d46daa46f5997fcd1f9aa77b45bda4
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-22 12:07:44 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (38.81%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,18 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n=len(nums)
-        if n<2:
-            return max(nums[0],0)
-        dp=[0]*(n+1)
-        dp[1]=nums[0]
-        res=0
-        for i in range(1,n-1):
-            dp[i+1]=max(dp[i],dp[i-1]+nums[i])
-        res=max(res,dp[n-1])
-        
-        dp=[0]*(n+1)
-        dp[2]=nums[1]
-        for i in range(2,n):
-            dp[i+1]=max(dp[i],dp[i-1]+nums[i])
-        res=max(res,dp[-1])
-        return res
+        if len(nums)<2:
+            return nums[0]
+        def getMoney(h):
+            n=len(h)
+            if n==0:
+                return 0
+            if n==1:
+                return h[0]
+            dp=[0]*(n+1)
+            dp[1],dp[2]=h[0],h[1]
+            for i in range(2,n+1):
+                dp[i]=max(dp[i-2]+h[i-1],dp[i-1])
+            return dp[-1]
+
+        return max(getMoney(nums[1:]),getMoney(nums[:-1]))
