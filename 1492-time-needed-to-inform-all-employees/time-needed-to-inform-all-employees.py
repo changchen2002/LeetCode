9512baa4d5bca2697a9f90a529e955d06c7732e5
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-25 18:35:46 -0700

    Time: 372 ms (12.02%) | Memory: 62.4 MB (5.16%) - LeetSync

--- 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.py
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.py
@@ -1,22 +1,13 @@
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
-        if n<2:
-            return informTime[headID]
-        
         adj=defaultdict(list)
         for e,m in enumerate(manager):
             if e!=headID:
                 adj[m].append(e)
 
-        time=informTime[headID]
-        heap=[(time,headID)]
-        while heap:
-            time,curE=heapq.heappop(heap)
-            for sub in adj[curE]:
-                heapq.heappush(heap,(time+informTime[sub],sub))
+        def dfs(e):
+            if not adj[e]:
+                return 0
+            return max(dfs(sub) for sub in adj[e])+informTime[e]
         
-        return time
-                
-
-        
-
+        return dfs(headID)
