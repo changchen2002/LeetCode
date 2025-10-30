60c80b54feefbf0259eea5fe2248df53d0bc7ba1
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-29 21:11:10 -0700

    Time: 602 ms (34.19%) | Memory: 69.9 MB (57.04%) - LeetSync

--- 2364-longest-path-with-different-adjacent-characters/longest-path-with-different-adjacent-characters.py
+++ 2364-longest-path-with-different-adjacent-characters/longest-path-with-different-adjacent-characters.py
@@ -1,21 +1,22 @@
 class Solution:
     def longestPath(self, parent: List[int], s: str) -> int:
         adj=defaultdict(list)
-        for i,val in enumerate(parent):
-            adj[val].append(i)
+        for i,p in enumerate(parent):
+            adj[p].append(i)
+        
         res=1
-        def dfs(node):
+        def dfs(cur):
             nonlocal res
-            a=b=0
-            for nei in adj[node]:
-                cur=dfs(nei)
-                if s[nei]==s[node]:
+            a,b=0,0
+            for nei in adj[cur]:
+                length=dfs(nei)
+                if s[nei]==s[cur]:
                     continue
-                if cur>a:
-                    a,b=cur,a
-                elif cur>b:
-                    b=cur
-            res=max(res,a+b+1) 
-            return a+1 
+                if length>a:
+                    a,b=length,a
+                elif length>b:
+                    b=length
+            res=max(res,a+b+1)
+            return a+1
         dfs(0)
         return res
