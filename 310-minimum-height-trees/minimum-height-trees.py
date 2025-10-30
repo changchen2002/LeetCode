599dc478b5762fb11468184d743019f66b7e9dde
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-10-29 19:53:25 -0700

    Time: 65 ms (31.16%) | Memory: 28.5 MB (45.08%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.py
+++ 310-minimum-height-trees/minimum-height-trees.py
@@ -1,21 +1,26 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         if n<=2:
-            return [i for i in range(n)]
-        adj=defaultdict(set)
+            return list(range(n))
+        adj=defaultdict(list)
+        indeg=[0]*n
         for u,v in edges:
-            adj[u].add(v)
-            adj[v].add(u)
-        remain=n
-        q=deque([i for i in range(n) if len(adj[i])==1])
-        while remain>2:
-            remain-=len(q)
+            adj[u].append(v)
+            adj[v].append(u)
+            indeg[v]+=1
+            indeg[u]+=1
+        
+        q=deque([i for i in range(n) if indeg[i]==1])
+        vis=set(q)
+        while q:
+            print(q)
             for _ in range(len(q)):
-                leaf=q.popleft()
-                nei=adj[leaf].pop()
-                adj[nei].remove(leaf)
-                if len(adj[nei])==1:
-                    q.append(nei)
-        return list(q)
-
-                
+                cur=q.popleft()
+                vis.add(cur)
+                for nei in adj[cur]:
+                    if nei not in vis:
+                        indeg[nei]-=1
+                        if indeg[nei]==1:
+                            q.append(nei)
+            if n-len(vis)<=2:
+                return list(q)
