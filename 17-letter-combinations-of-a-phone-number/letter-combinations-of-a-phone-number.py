11b9d4d0283de98af63909b3b9e3de09b3516d54
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-11-19 15:02:40 -0800

    Time: 3 ms (5.45%) | Memory: 17.9 MB (22.43%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,20 +1,23 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return []
         mapping={
-            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
-            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+            '2':['a','b','c'],
+            '3':['d','e','f'],
+            '4':['g','h','i'],
+            '5':['j','k','l'],
+            '6':['m','n','o'],
+            '7':['p','q','r','s'],
+            '8':['t','u','v'],
+            '9':['w','x','y','z']
         }
         res=[]
-        def backtrack(i,path):
-            if i==len(digits):
-                res.append("".join(path))
+        def dfs(cur,idx):
+            if idx==len(digits):
+                res.append(cur)
                 return
-            for letter in mapping[digits[i]]:
-                path.append(letter)
-                backtrack(i+1,path)
-                path.pop()
-            return
-        backtrack(0,[])
+            d=digits[idx]
+            for letter in mapping[d]:
+                dfs(cur+letter,idx+1)
+        
+        dfs("",0)
         return res
