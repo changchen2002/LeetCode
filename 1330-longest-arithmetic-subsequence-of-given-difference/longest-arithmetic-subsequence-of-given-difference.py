b6561273dd8a6deb065bb5a134b35b5b96d8f991
Chang2002 <151889980+changchen2002@users.noreply.github.com>
2025-11-27 15:31:15 -0800

    Time: 135 ms (9.69%) | Memory: 28.8 MB (51.86%) - LeetSync

---
+++ 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestSubsequence(self, arr: List[int], difference: int) -> int:
+        hashmap=defaultdict(int)
+        res=0
+        for i,num in enumerate(arr):
+            if num-difference in hashmap:
+                hashmap[num]=max(hashmap[num],hashmap[num-difference]+1)
+            else:
+                hashmap[num]=1
+            res=max(res,hashmap[num])
+        return res
